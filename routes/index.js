8475f728932c81c4c4520cde525b1e368990ce67
Shashank Garg <shashankgarg7@gmail.com>
2014-11-08 11:24:25 -0800

    Download module

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+// Get Download page
+router.get('/download', function(req, res) {
+  res.render('download', { title: 'Express' });
+});
+
 module.exports = router;
