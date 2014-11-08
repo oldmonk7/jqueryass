119cec6755e165fcd2641211ea2df4faf0e4bf51
Maulik Fojdar <maulik712@gmail.com>
2014-11-08 15:11:52 -0800

    Adding empty routes and views
    Added routes and views for mobile, bigdata and iot pages.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,21 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+//Get Mobile Page
+router.get('/mobile', function(req, res) {
+  res.render('mobile', { title: 'Express' });
+});
+
+//Get Big Data Page
+router.get('/bigdata', function(req, res) {
+  res.render('bigdata', { title: 'Express' });
+});
+
+//Get IoT Page
+router.get('/iot', function(req, res) {
+  res.render('iot', { title: 'Express' });
+});
+
 // Get Download page
 router.get('/download', function(req, res) {
   res.render('download', { title: 'Express' });
