607fa8fe8a52eda100c2e6f9b8ae4066652ee415
Shashank Garg <shashankgarg7@gmail.com>
2014-11-08 17:54:21 -0800

    changed title

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pulp Fiction Inc.' });
 });
 
 //Get Mobile Page
 router.get('/mobile', function(req, res) {
-  res.render('mobile', { title: 'Express' });
+  res.render('mobile', { title: 'Pulp Fiction Inc.' });
 });
 
 //Get Big Data Page
 router.get('/bigdata', function(req, res) {
-  res.render('bigdata', { title: 'Express' });
+  res.render('bigdata', { title: 'Pulp Fiction Inc.' });
 });
 
 //Get IoT Page
 router.get('/iot', function(req, res) {
-  res.render('iot', { title: 'Express' });
+  res.render('iot', { title: 'Pulp Fiction Inc.' });
 });
 
 //Get location Page
 router.get('/location', function(req, res) {
-  res.render('location', { title: 'Express' });
+  res.render('location', { title: 'Pulp Fiction Inc.' });
 });
 
 
 // Get Download page
 router.get('/download', function(req, res) {
-  res.render('download', { title: 'Express' });
+  res.render('download', { title: 'Pulp Fiction Inc.' });
 });
 
 module.exports = router;
